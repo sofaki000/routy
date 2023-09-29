4d61307ece3d343113bdc5a5a47b077a7976892e
Sofia Kardami <sofiakardami@Sofias-MacBook-Air.local>
2023-09-29 14:37:10 +0200

    Documentation for comparison class

--- routy/comparison.py
+++ routy/comparison.py
@@ -1,13 +1,11 @@
-from routy.plotUtilities.plotRoute import plot_route, plot_multiple_routes_for_same_coordinates
+from routy.plotUtilities.plotRoute import plot_multiple_routes_for_same_coordinates, plot_route
 
 
 class Comparison:
     """
-    Instantiate a comparison operation.
-    Numbers will be multiplied by the given multiplier.
-
-    :param multiplier: The multiplier.
-    :type multiplier: int
+    Instantiate a comparison operation. This class is the starting point for comparing your
+    routing models.
+    :param None
     """
 
     def __init__(self):
@@ -15,8 +13,16 @@ class Comparison:
 
     def plot_route(self, coordinates, routes, filename="route"):
         '''
-        coordinates: list size seq_len, (x,y), size: [seq_len, 2]
-        routes: list with size [seq_len]
+        Plot a single route based on coordinates and routes.
+
+        :param coordinates: List of coordinates [(x, y)] for each point on the route.
+        :type coordinates: list of tuples
+        :param routes: List of indices representing the route.
+        :type routes: list
+        :param filename: Name of the output plot file (default: "route.png").
+        :type filename: str
+         :Examples:
+        plot_route([(0, 0), (1, 1), (2, 2)], [0, 1, 2], "example_route.png")
         '''
 
         seq_len = len(routes)
@@ -27,7 +33,17 @@ class Comparison:
 
     def plot_routes(self,coordinates, routes, filename="routes"):
         '''
+        Plot multiple routes for the same set of coordinates.
 
+        :param coordinates: List of coordinates [(x, y)] for each point on the routes.
+        :type coordinates: list of tuples
+        :param routes: List of lists, each representing a route with indices.
+        :type routes: list of lists
+        :param filename: Name of the output plot file (default: "routes.png").
+        :type filename: str
+
+         :Examples:
+        plot_routes([(0, 0), (1, 1), (2, 2)], [[0, 1, 2], [2, 1, 0]], "example_routes.png")
         '''
         seq_len = len(routes[0])
         assert seq_len == len(coordinates)
